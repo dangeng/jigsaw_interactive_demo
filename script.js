2cdc630a26f3bb3cad5f619f11604bc014c5f232
dangeng <dangengdg@gmail.com>
2024-02-15 22:03:05 -0500

    New puzzles, result of poll

--- script.js
+++ script.js
@@ -34,31 +34,92 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var link = document.createElement("a");
         link.href = 'index.html?name=duck.rabbit.0059'
-        link.text = 'Duck Rabbit'
+        link.text = 'duck.rabbit.0059'
         document.body.appendChild(link);
         document.body.appendChild(document.createElement("br"));
 
         var link = document.createElement("a");
-        link.href = 'index.html?name=marilyn.einstein.0408'
-        link.text = 'Marilyn Einstein'
+        link.href = 'index.html?name=duck.rabbit.0237'
+        link.text = 'duck.rabbit.0237'
         document.body.appendChild(link);
         document.body.appendChild(document.createElement("br"));
 
         var link = document.createElement("a");
-        link.href = 'index.html?name=marilyn.einstein.0488'
-        link.text = 'Marilyn Einstein 2'
+        link.href = 'index.html?name=duck.rabbit.0413'
+        link.text = 'duck.rabbit.0413'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=fruit.monkey.0208'
+        link.text = 'fruit.monkey.0208'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=fruit.monkey.0235'
+        link.text = 'fruit.monkey.0235'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=kitten.puppy.0164'
+        link.text = 'kitten.puppy.0164'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=kitten.puppy.0510'
+        link.text = 'kitten.puppy.0510'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=kitten.puppy.0645'
+        link.text = 'kitten.puppy.0645'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=kitten.puppy.0754'
+        link.text = 'kitten.puppy.0754'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=marilyn.einstein.0344'
+        link.text = 'marilyn.einstein.0344'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=marilyn.einstein.0744'
+        link.text = 'marilyn.einstein.0744'
         document.body.appendChild(link);
         document.body.appendChild(document.createElement("br"));
 
         var link = document.createElement("a");
         link.href = 'index.html?name=plants.deer.1005'
-        link.text = 'Plants Deer'
+        link.text = 'plants.deer.1005'
         document.body.appendChild(link);
         document.body.appendChild(document.createElement("br"));
 
         var link = document.createElement("a");
-        link.href = 'index.html?name=plants.marilyn.0229'
-        link.text = 'Plants Marilyn'
+        link.href = 'index.html?name=plants.einstein.0403'
+        link.text = 'plants.einstein.0403'
         document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=plants.marilyn.0189'
+        link.text = 'plants.marilyn.0189'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
+
+        var link = document.createElement("a");
+        link.href = 'index.html?name=washington.lincoln.0274'
+        link.text = 'washington.lincoln.0274'
+        document.body.appendChild(link);
+        document.body.appendChild(document.createElement("br"));
     }
 });
